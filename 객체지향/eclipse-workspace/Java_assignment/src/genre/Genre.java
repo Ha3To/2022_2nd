7b8b4e5f2c03567df1c3b91ac7c6f200aa3b6ea1
Ha3To <97393619+Ha3To@users.noreply.github.com>
2022-12-11 22:23:20 +0900

    Update Genre.java

--- 객체지향/eclipse-workspace/Java_assignment/src/genre/Genre.java
+++ 객체지향/eclipse-workspace/Java_assignment/src/genre/Genre.java
@@ -1,18 +1,18 @@
 package genre;
 
 public class Genre {
-	private String part;
+	private String title;
 	private String rc;
 
 	public Genre() {}
 	
-	public Genre(String part, String rc) {
-		this.part = part;
+	public Genre(String title, String rc) {
+		this.title = title;
 		this.rc = rc;
 	}
 	
 	public void showGenreInfo() {
-		System.out.println("분야: " + part);
+		System.out.println("제목: " + title);
 		System.out.println("감독 및 주연: " + rc);
 		System.out.println("");
 	}
